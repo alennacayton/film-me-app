79a1b443b7e47b07bba0f51c01c52457b9d7b69c
cheskaroque <francheska_roque@dlsu.edu.ph>
2021-08-17 00:59:54 +0800

    sign-up and log-in/landing front end

---
+++ app/src/main/java/ph/com/filmmeapp/LandingActivity.java
@@ -0,0 +1,14 @@
+package ph.com.filmmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
