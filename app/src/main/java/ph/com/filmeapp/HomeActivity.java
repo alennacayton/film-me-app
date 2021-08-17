3085d12efabe4178c5b5455ecbbb155bee359a8d
alenna <rainiiicorn@gmail.com>
2021-08-17 22:13:30 +0800

    added navigation bar, intents, home page ui not yet done

---
+++ app/src/main/java/ph/com/filmeapp/HomeActivity.java
@@ -0,0 +1,56 @@
+package ph.com.filmeapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Initialize and Assign Variable
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setItemTextColor(ColorStateList.valueOf(Color.BLACK));
+
+
+        // Set Home Selected
+
+        bottomNavigationView.setSelectedItemId(R.id.nb_home);
+
+        // Perform ItemSelectedListener
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()){
+
+                    case R.id.nb_home:
+                        return true;
+                    case R.id.nb_add:
+                        startActivity(new Intent(getApplicationContext(), AddRecomActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+
+                return false;
+            }
+        });
+    }
+}
