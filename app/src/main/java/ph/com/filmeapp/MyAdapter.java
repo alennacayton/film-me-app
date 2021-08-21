d0f79c12cbc61267e1fd7de9f26786971d78d395
cheskaroque <francheska_roque@dlsu.edu.ph>
2021-08-22 00:58:52 +0800

    latest

--- app/src/main/java/ph/com/filmeapp/MyAdapter.java
+++ app/src/main/java/ph/com/filmeapp/MyAdapter.java
@@ -1,4 +1,42 @@
 package ph.com.filmeapp;
 
-public class MyAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    //Context context;
+
+    //ArrayList<>
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+        }
+    }
 }
